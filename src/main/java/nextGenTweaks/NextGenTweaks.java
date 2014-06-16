1c3061d8acdea85cf2ad307e155420b0f8ec10e4
AEnterprise <aenterprise2@gmail.com>
2014-06-16 15:52:09 +0200

    made the functions for removing crafting recepies did if for the ender quarry as a demonstration

--- src/main/java/nextGenTweaks/NextGenTweaks.java
+++ src/main/java/nextGenTweaks/NextGenTweaks.java
@@ -1,13 +1,21 @@
 package nextGenTweaks;
 
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
 import nextGenTweaks.proxy.CommonProxy;
 
-@Mod(modid = "nextgentweaks" ,name="NextGen Tweaks", version = "@VERSION@", dependencies = "required-after:BuildCraft|Energy@{6.0.16}")
+import java.util.ArrayList;
+
+@Mod(modid = "nextgentweaks" ,name="NextGen Tweaks", version = "@VERSION@", dependencies = "required-after:BuildCraft|Energy@{6.0.16};required-after:ExtraUtilities@{1.1.0b}")
 public class NextGenTweaks {
 
     @Instance
@@ -19,11 +27,29 @@ public class NextGenTweaks {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
 
+
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
+    public void load(FMLInitializationEvent event) {
+        if (Loader.isModLoaded("ExtraUtilities")) {
+            Block enderQuarry = GameRegistry.findBlock("ExtraUtilities", "enderQuarry");
+            removeRecepie(new ItemStack(enderQuarry));
+            System.out.println();
+        }
+    }
 
+
+    public void removeRecepie(ItemStack stack){
+        ArrayList recepieList = (ArrayList) CraftingManager.getInstance().getRecipeList();
+        for (int t = 0; t < recepieList.size(); t++) {
+            IRecipe recipe = (IRecipe) recepieList.get(t);
+            ItemStack recipeResult = recipe.getRecipeOutput();
+            if (ItemStack.areItemStacksEqual(stack, recipeResult)){
+                recepieList.remove(t);
+                return;
+            }
+        }
     }
 
 }
