d802983792c42e1c2284ff6993edc16b58bd3995
iTitus <iTitus@users.noreply.github.com>
2014-06-26 16:32:27 +0200

    Removed unnecessary methods and fields

--- src/main/java/nextGenTweaks/NextGenTweaks.java
+++ src/main/java/nextGenTweaks/NextGenTweaks.java
@@ -1,49 +1,38 @@
 package nextGenTweaks;
 
-import cpw.mods.fml.common.Loader;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
+import java.util.List;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
-import java.util.ArrayList;
-
-@Mod(modid = "nextgentweaks" ,name="NextGen Tweaks", version = "@VERSION@", dependencies = "required-after:BuildCraft|Energy@{6.0.16};required-after:ExtraUtilities@{1.1.0b}")
+@Mod(modid = "nextgentweaks", name = "NextGen Tweaks", version = "@VERSION@", dependencies = "required-after:BuildCraft|Energy@{6.0.16};required-after:ExtraUtilities@{1.1.0b}")
 public class NextGenTweaks {
 
-    @Instance
-    public static NextGenTweaks instance;
+	@EventHandler
+	public void load(FMLInitializationEvent event) {
+		if (Loader.isModLoaded("ExtraUtilities")) {
+			Block enderQuarry = GameRegistry.findBlock("ExtraUtilities", "enderQuarry");
+			removeRecipe(new ItemStack(enderQuarry));
+		}
+	}
 
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-
-
-    }
-
-    @Mod.EventHandler
-    public void load(FMLInitializationEvent event) {
-        if (Loader.isModLoaded("ExtraUtilities")) {
-            Block enderQuarry = GameRegistry.findBlock("ExtraUtilities", "enderQuarry");
-            removeRecipe(new ItemStack(enderQuarry));        
-        }
-    }
-
-
-    public void removeRecipe(ItemStack stack){
-        ArrayList recipeList = (ArrayList) CraftingManager.getInstance().getRecipeList();
-        for (int t = 0; t < recepieList.size(); t++) {
-            IRecipe recipe = (IRecipe) recepieList.get(t);
-            ItemStack recipeResult = recipe.getRecipeOutput();
-            if (ItemStack.areItemStacksEqual(stack, recipeResult)){
-                recepieList.remove(t);
-                return;
-            }
-        }
-    }
+	public void removeRecipe(ItemStack stack) {
+		List<IRecipe> recipeList = CraftingManager.getInstance().getRecipeList();
+		for (int i = 0; i < recipeList.size(); i++) {
+			IRecipe recipe = recipeList.get(i);
+			ItemStack recipeResult = recipe.getRecipeOutput();
+			if (ItemStack.areItemStacksEqual(stack, recipeResult)) {
+				recipeList.remove(i);
+				return;
+			}
+		}
+	}
 
 }
